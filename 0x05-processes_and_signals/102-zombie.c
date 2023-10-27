80ca38c7ed41d6887d01435d7b97ac2fd1f7fc31
tebogo-gema <gematebogo@gmail.com>
2023-10-27 06:44:30 -0700

    0x05-processes_and_signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,24 @@
+/*
+ * File: 102-zombie.c
+ * Auth: Tebogo Gema
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
