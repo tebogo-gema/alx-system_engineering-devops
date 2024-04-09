63aa628175d2477b9fd873d88f2c1f35f05c75d9
tebogo-gema <gematebogo@gmail.com>
2024-04-09 13:05:30 -0700

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""
-Script to print hot posts on a given Reddit subreddit.
-"""
 
-import requests
+import requests as r
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
+    """Print top 10 post given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    # Define headers for the HTTP request, including User-Agent
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
     }
-
-    # Define parameters for the request, limiting the number of posts to 10
-    params = {
+    param = {
         "limit": 10
     }
-
-    # Send a GET request to the subreddit's hot posts page
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    # Check if the response status code indicates a not-found error (404)
+    response = r.get(url, headers=headers, params=param, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-
-    # Parse the JSON response and extract the 'data' section
     results = response.json().get("data")
-
-    # Print the titles of the top 10 hottest posts
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    [print(top.get("data").get("title")) for top in results.get("children")]
